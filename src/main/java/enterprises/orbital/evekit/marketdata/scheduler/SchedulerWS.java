8d951f6ff8a0a3479e63bdcb04c876872d0f465d
Mark Astley <deadlybulb@orbital.enterprises>
2016-05-05 20:46:18 -0400

    - support submitting orders over REST to avoid DB contention

--- src/main/java/enterprises/orbital/evekit/marketdata/scheduler/SchedulerWS.java
+++ src/main/java/enterprises/orbital/evekit/marketdata/scheduler/SchedulerWS.java
@@ -1,5 +1,7 @@
 package enterprises.orbital.evekit.marketdata.scheduler;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -7,16 +9,23 @@ import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import enterprises.orbital.base.OrbitalProperties;
 import enterprises.orbital.base.PersistentProperty;
+import enterprises.orbital.db.ConnectionFactory.RunInVoidTransaction;
+import enterprises.orbital.evekit.marketdata.EveKitMarketDataProvider;
 import enterprises.orbital.evekit.marketdata.Instrument;
+import enterprises.orbital.evekit.marketdata.Order;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
@@ -70,6 +79,103 @@ public class SchedulerWS {
     }
     if (next == null) return Response.status(Status.NOT_FOUND).build();
     return Response.ok().entity(next).build();
+  }
+
+  @Path("/store")
+  @POST
+  @ApiOperation(
+      value = "Populate an order for an instrument.")
+  @ApiResponses(
+      value = {
+          @ApiResponse(
+              code = 200,
+              message = "Population successful"),
+          @ApiResponse(
+              code = 500,
+              message = "Internal error"),
+      })
+  public Response storeOrder(
+                             @Context HttpServletRequest request,
+                             @QueryParam("regionid") @ApiParam(
+                                 name = "regionid",
+                                 required = true,
+                                 value = "Region where order is located") final int regionID,
+                             @QueryParam("typeid") @ApiParam(
+                                 name = "typeid",
+                                 required = true,
+                                 value = "Type ID of order") final int typeID,
+                             @QueryParam("orderid") @ApiParam(
+                                 name = "orderid",
+                                 required = true,
+                                 value = "Order ID") final long orderID,
+                             @QueryParam("buy") @ApiParam(
+                                 name = "buy",
+                                 required = true,
+                                 value = "True if the order is a buy, false otherwise") final boolean buy,
+                             @QueryParam("issued") @ApiParam(
+                                 name = "issued",
+                                 required = true,
+                                 value = "Order issue date (milliseconds UTC)") final long issued,
+                             @QueryParam("price") @ApiParam(
+                                 name = "price",
+                                 required = true,
+                                 value = "Order price (converted to big decimal)") final String price,
+                             @QueryParam("volumeentered") @ApiParam(
+                                 name = "volumeentered",
+                                 required = true,
+                                 value = "Order volume entered") final int volumeEntered,
+                             @QueryParam("minvolume") @ApiParam(
+                                 name = "minvolume",
+                                 required = true,
+                                 value = "Minimum order volume") final int minVolume,
+                             @QueryParam("volume") @ApiParam(
+                                 name = "volume",
+                                 required = true,
+                                 value = "Order volume") final int volume,
+                             @QueryParam("range") @ApiParam(
+                                 name = "range",
+                                 required = true,
+                                 value = "Order range") final String range,
+                             @QueryParam("locationid") @ApiParam(
+                                 name = "locationid",
+                                 required = true,
+                                 value = "Location where order was entered") final long locationID,
+                             @QueryParam("duration") @ApiParam(
+                                 name = "duration",
+                                 required = true,
+                                 value = "Order duration") final int duration) {
+    final BigDecimal priceValue = BigDecimal.valueOf(Double.valueOf(price).doubleValue()).setScale(2, RoundingMode.HALF_UP);
+    final long at = OrbitalProperties.getCurrentTime();
+    // Construct new potential order
+    final Order check = new Order(regionID, typeID, orderID, buy, issued, priceValue, volumeEntered, minVolume, volume, range, locationID, duration);
+    try {
+      EveKitMarketDataProvider.getFactory().runTransaction(new RunInVoidTransaction() {
+        @Override
+        public void run() throws Exception {
+          // Check whether we already know about this order
+          Order existing = Order.get(at, regionID, typeID, orderID);
+          // If the order exists and has changed, then evolve. Otherwise store the new order.
+          if (existing != null) {
+            // Existing, evolve if changed
+            if (!existing.equivalent(check)) {
+              // Evolve
+              existing.evolve(check, at);
+              Order.update(existing);
+              Order.update(check);
+            }
+          } else {
+            // New entity
+            check.setup(at);
+            Order.update(check);
+          }
+        }
+      });
+    } catch (Exception e) {
+      log.log(Level.SEVERE, "DB error storing order, failing", e);
+      return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+    }
+    // Order accepted
+    return Response.ok().build();
   }
 
 }
